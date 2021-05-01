83d38e4191fb563ad7aaef82dc89211c280a6967
Mahesh Deshpande <mdmahesh18@gmail.com>
2021-05-01 10:23:19 -0500

    activate link component fixed for env

--- src/auth/Activate.js
+++ src/auth/Activate.js
@@ -5,6 +5,7 @@ import axios from 'axios'
 import {ToastContainer, toast} from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.min.css'
 import jwt from 'jsonwebtoken'
+require('dotenv').config();
 //import { post } from '../../../server/Routes/Auth'
 import '../style.css'
 
